2e54e58aac287099a35c179cb8e4929e7542c668
Rostyslav <rostislavkpi@gmail.com>
2024-08-17 23:27:36 +0300

    make build

---
+++ index.9b70c866.js
@@ -0,0 +1,2 @@
+document.querySelector("ul").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item__link");t&&(document.getElementById("largeImg").src=t.href)});
+//# sourceMappingURL=index.9b70c866.js.map
